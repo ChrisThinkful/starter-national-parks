c8bc5d6b8c541750fd8b3b4cd4e830dcad0e719b
Chris <78775458+ChrisThinkful@users.noreply.github.com>
2021-04-01 10:16:17 -0500

    Update index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+// get a list of all <h3> elements
+const heading3List = document.querySelectorAll("h3");
+
+// iterate over the list and print each one
+for (let element of heading3List.values()) {
+    console.log(element);
+}
