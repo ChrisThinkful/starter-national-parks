943d3f2f1c37c5af5a9bff9627dcfb80359880b8
Chris <78775458+ChrisThinkful@users.noreply.github.com>
2021-04-01 13:25:17 -0500

    displaying descriptions

--- index.js
+++ index.js
@@ -5,3 +5,10 @@ const heading3List = document.querySelectorAll("h3");
 for (let element of heading3List.values()) {
     console.log(element);
 }
+
+const descriptions = document.querySelectorAll(".description-display");
+
+for (let desc of descriptions.values()) {
+    let content = desc.innerText;
+    console.log(content);
+}
